c7421ffa18121069246bc1219a1729e6861f440e
Ignacio Lencina <nacholencina60@gmail.com>
2024-08-05 15:14:13 -0300

    Agrego validaciones de sesion en el navbar

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,6 +1,25 @@
 import { Link, NavLink } from "react-router-dom";
+import { useSession } from "../../stores/useSession.js";
+import Swal from "sweetalert2";
 
 const Header = () => {
+  const { user, isLoggedIn, logout } = useSession();
+
+  const handleLogout = async () => {
+    const action = await Swal.fire({
+      title: "Attention",
+      text: "Are you sure you want to logout?",
+      icon: "info",
+      confirmButtonText: "Logout",
+      showCancelButton: true,
+      cancelButtonText: "Cancel",
+    });
+
+    if (action.isConfirmed) {
+      logout();
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-lg bg-danger" data-bs-theme="dark">
       <div className="container">
@@ -11,7 +30,7 @@ const Header = () => {
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
-          className="navbar-toggler"
+          className="navbar-toggler ms-auto me-3"
           data-bs-target="#navbarNav"
           data-bs-toggle="collapse"
           type="button"
@@ -27,31 +46,40 @@ const Header = () => {
                 }}
                 to="/"
               >
-                Inicio
+                Home
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink
-                className={({ isActive }) => {
-                  return isActive ? "nav-link active" : "nav-link";
-                }}
-                to="/login"
-              >
-                Iniciar Sesión
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className={({ isActive }) => {
-                  return isActive ? "nav-link active" : "nav-link";
-                }}
-                to="/admin"
-              >
-                Admin
-              </NavLink>
-            </li>
+            {!isLoggedIn && (
+              <li className="nav-item">
+                <NavLink
+                  className={({ isActive }) => {
+                    return isActive ? "nav-link active" : "nav-link";
+                  }}
+                  to="/login"
+                >
+                  Login
+                </NavLink>
+              </li>
+            )}
+            {isLoggedIn && user.isAdmin && (
+              <li className="nav-item">
+                <NavLink
+                  className={({ isActive }) => {
+                    return isActive ? "nav-link active" : "nav-link";
+                  }}
+                  to="/admin"
+                >
+                  Admin
+                </NavLink>
+              </li>
+            )}
           </ul>
         </div>
+        {isLoggedIn && (
+          <button className="btn btn-outline-light" onClick={handleLogout}>
+            Logout
+          </button>
+        )}
       </div>
     </nav>
   );
